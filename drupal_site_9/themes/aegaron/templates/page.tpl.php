153dc25a84e9c87b51f38c26047046c17a2a9ac9
Chris Patterson <chris@oit.ucla.edu>
2014-05-16 20:10:37 -0700

    theme styling

--- drupal_site_9/themes/aegaron/templates/page.tpl.php
+++ drupal_site_9/themes/aegaron/templates/page.tpl.php
@@ -81,7 +81,7 @@
           <div class="container">
       <?php if ($logo): ?>
         <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
-          <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+          <img src="<?php print $logo; ?>" alt="<?php print t('AEGARON - Ancient Egyptian Architecture Online'); ?>" />
         </a>
       <?php endif; ?>
 
